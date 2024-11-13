b7b072eb92b0ceab2e4af4d0ae0fd832dd3f4ec6
Paul <mothcar@gmail.com>
2024-11-13 18:20:57 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Webcam from "react-webcam";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <Webcam />
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card"></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
