03601330c1a33dc22f89fe59b803a757ed453eab
Paul <mothcar@gmail.com>
2024-11-13 18:58:11 +0900

    1. toggle button 2. css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [face, setFace] = useState(true);
+
+  const clickHandler = () => {
+    setFace(!face);
+  };
 
   return (
     <>
       <div className="webcamCapture">
-        <Webcam />
+        <Webcam mirrored={face} className="mycam"/>
       </div>
+      <button onClick={clickHandler}>반전</button>
       <h1>Vite + React</h1>
       <div className="card"></div>
       <p className="read-the-docs">
